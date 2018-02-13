337dc86bb65fa1ef34420e852baef229ac301f9e
Jarvis <utkarsh@172-16-15-25.DYNAPOOL.NYU.EDU>
2018-02-13 15:17:37 -0500

    Creadted sub functions

--- dp2.ino
+++ dp2.ino
@@ -1,19 +1,24 @@
 #include <Servo.h> 
- 
-int servoPin = 5;
-
-
-
 #include <Wire.h>
 #include <LIDARLite.h>
-int cal_cnt = 0;
 
+
+
+
+ 
+int servoPin = 5;
+int arr1[40];
+int arr2[40];
+int arr3[40];
+int dist;
+int cal_cnt = 0;
+int angle = 0;   // servo position in degrees 
 
 LIDARLite lidarLite;
-
 Servo servo;  
  
-int angle = 0;   // servo position in degrees 
+
+
  
 void setup() 
 { 
@@ -28,24 +33,36 @@ lidarLite.configure(0); // Change this number to try out alternate configuration
 void loop() 
 { 
 
+get_data(1,40);   // (direction,till angle)
+get_data(-1,40);   // (direction,till angle)
+//Print_to_serial(arr1,40);
+//Print_to_serial(arr2,40);
+take_diff(40);
+
+} 
 
 
-  int dist;
+void Print_to_serial(int TheArr[], int n)
+{
+  Serial.print("{");
+for(int i = 0; i < n; i++)
+{
+  Serial.print(TheArr[i]);
+  if(i<(n-1)){
+  Serial.print(",");}
+}
+  Serial.print("} ");
+  Serial.println("");
+  }
 
-  // At the beginning of every 100 readings,
-  // take a measurement with receiver bias correction
- 
 
-
-
-
-
-  //delay(10);
-  //tix = millis;
-  // scan from 0 to 180 degrees
-  for(angle = 0; angle < 40; angle++)  
+void get_data(int d, int p)
+{
+  if(d==1){
+    
+  for(angle = 0; angle < p; angle++)  
   {                
-      // Increment reading counter
+ 
   cal_cnt++;
   cal_cnt = cal_cnt % 100;                  
     servo.write(angle); 
@@ -54,13 +71,15 @@ void loop()
   } else {
     dist = lidarLite.distance(false); // Without bias correction
   }
+  arr1[angle] = dist;
     
-  Serial.println(dist);
-                
-    delay(10);                   
+  //Serial.println(dist);            
+    delay(10);                    
   } 
-  // now scan back from 180 to 0 degrees
-  for(angle = 40; angle > 0; angle--)    
+}
+  if(d==-1){
+    
+  for(angle = p; angle > 0; angle--)    
   {        
       // Increment reading counter
   cal_cnt++;
@@ -71,8 +90,30 @@ void loop()
   } else {
     dist = lidarLite.distance(false); // Without bias correction
   }
-  Serial.println(dist);
+  //Serial.println(dist);
+
+  arr2[angle-1] = dist;
             
     delay(10);       
   } 
-} 
+}
+
+
+}
+
+void take_diff(int p)
+{
+for(int n =  0; n < p; n++)  
+  {    
+    arr3[n] = arr2[n]-arr1[n] ;           
+  }
+  
+Print_to_serial(arr3, 40);
+  
+}
+
+
+
+
+
+
